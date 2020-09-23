9d055f4602ce5bee2f08e8d9053bb625ff09142c
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-09-23 09:21:38 +0200

    Added BindableBase, and ViewModelBase

---
+++ HTKKlub.Desktop.Gui/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+namespace HTKKlub.Desktop.Gui.ViewModels.Base
+{
+    /// <summary>
+    /// Base class for ViewModels
+    /// </summary>
+    public abstract class ViewModelBase : BindableBase
+    {
+        #region Methods
+        /// <summary>
+        /// Calls the <see cref="LoadAllAsync"/> method used to load all viewmodel data.
+        /// </summary>
+        public virtual async Task InitializeAsync()
+        {
+            // Load suppliers
+            await LoadAllAsync();
+        }
+
+        /// <summary>
+        /// Loads all data from the database, and assigns it to the observable collections.
+        /// </summary>
+        protected abstract Task LoadAllAsync();
+        #endregion
+    }
+}
