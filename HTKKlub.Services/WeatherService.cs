a902ecdb73797b88f655406555a299e7de4432fa
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-09-23 13:32:59 +0200

    added endpoint

--- HTKKlub.Services/WeatherService.cs
+++ HTKKlub.Services/WeatherService.cs
@@ -1,7 +1,5 @@
 ﻿using HTKKlub.Services.Base;
 
-using System;
-
 namespace HTKKlub.Services
 {
     /// <summary>
@@ -9,5 +7,9 @@ namespace HTKKlub.Services
     /// </summary>
     public class WeatherService : WebServiceBase
     {
+        // URI to the endpoint.
+        // And of course its smart to include the api key in here.
+        // Because this will definitely not end up in a public repository on Github!
+        protected const string endpoint = "https://api.openweathermap.org/data/2.5/onecall?lat=55.71&lon=9.54&exclude=hourly,current,minutely&appid=";
     }
 }
