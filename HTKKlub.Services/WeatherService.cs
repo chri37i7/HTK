2f2d9d6664e4918bddc4694a31e42c89b60dc76a
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-09-23 10:35:30 +0200

    Added weather service and webservicebase

---
+++ HTKKlub.Services/WeatherService.cs
@@ -0,0 +1,13 @@
+﻿using HTKKlub.Services.Base;
+
+using System;
+
+namespace HTKKlub.Services
+{
+    /// <summary>
+    /// Class for getting weather information
+    /// </summary>
+    public class WeatherService : WebServiceBase
+    {
+    }
+}
