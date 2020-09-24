b0918f1a7aaffc99695d35d7f16758a853b5f68f
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-09-24 14:37:16 +0200

    More Validations

--- HTK.Entities/Models/Member.cs
+++ HTK.Entities/Models/Member.cs
@@ -1,27 +1,200 @@
-﻿using System;
+﻿using HTK.Utilities;
+using System;
 using System.Collections.Generic;
 
 namespace HTK.Entities.Models
 {
+    /// <summary>
+    /// Represents a <see cref="Member"/> of HTK
+    /// </summary>
     public partial class Member
     {
+        #region Fields
+        protected int pkMemberId;
+        protected string firstname;
+        protected string lastname;
+        protected string address;
+        protected string email;
+        protected string phone;
+        protected DateTime birthdate; 
+        #endregion
+
+        #region Constructor
         public Member()
         {
             Rankings = new HashSet<Ranking>();
             ReservationFkFirstMember = new HashSet<Reservation>();
             ReservationFkSecondMember = new HashSet<Reservation>();
+        } 
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Database id of the <see cref="Member"/>
+        /// </summary>
+        public virtual int PkMemberId
+        {
+            get { return pkMemberId; }
+            set
+            {
+                if(pkMemberId != value)
+                {
+                    (bool isValid, string errorMessage) = Validations.ValidateIsIntNegative(value);
+                    if(isValid)
+                    {
+                        pkMemberId = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(PkMemberId));
+                    }
+                }
+            }
         }
 
-        public int PkMemberId { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string Address { get; set; }
-        public string Email { get; set; }
-        public DateTime Birthdate { get; set; }
-        public string Phone { get; set; }
+        /// <summary>
+        /// Firstname of the <see cref="Member"/>
+        /// </summary>
+        public virtual string Firstname
+        {
+            get { return firstname; }
+            set
+            {
+                if(firstname != value)
+                {
+                    (bool isValid, string errorMessage) = Validations.ValidateIsStringNull(value);
+                    if(isValid)
+                    {
+                        firstname = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(Firstname));
+                    }
+                }
+            }
+        }
 
+        /// <summary>
+        /// Lastname of the <see cref="Member"/>
+        /// </summary>
+        public virtual string Lastname
+        {
+            get { return lastname; }
+            set
+            {
+                if(lastname != value)
+                {
+                    (bool isValid, string errorMessage) = Validations.ValidateIsStringNull(value);
+                    if(isValid)
+                    {
+                        lastname = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(Lastname));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Address of the <see cref="Member"/>
+        /// </summary>
+        public virtual string Address
+        {
+            get { return address; }
+            set
+            {
+                if(address != value)
+                {
+                    (bool isValid, string errorMessage) = Validations.ValidateIsStringNull(value);
+                    if(isValid)
+                    {
+                        address = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(Address));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Email of the <see cref="Member"/>
+        /// </summary>
+        public virtual string Email
+        {
+            get { return email; }
+            set
+            {
+                if(email != value)
+                {
+                    (bool isValid, string errorMessage) = Validations.ValidateIsStringNull(value);
+                    if(isValid)
+                    {
+                        email = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(Email));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Birthdate of the <see cref="Member"/>
+        /// </summary>
+        public virtual DateTime Birthdate
+        {
+            get { return birthdate; }
+            set
+            {
+                if(birthdate != value)
+                {
+                    (bool isValid, string errorMessage) = Validations.ValidateIsDateBefore(value, DateTime.Now);
+                    if(isValid)
+                    {
+                        birthdate = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(Birthdate));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Phone of the <see cref="Member"/>
+        /// </summary>
+        public virtual string Phone
+        {
+            get { return phone; }
+            set
+            {
+                if(phone != value)
+                {
+                    (bool isValid, string errorMessage) = Validations.ValidateIsStringNull(value);
+                    if(isValid)
+                    {
+                        phone = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(Phone));
+                    }
+                }
+            }
+        } 
+        #endregion
+
+        #region Navigation Properties
         public virtual ICollection<Ranking> Rankings { get; set; }
         public virtual ICollection<Reservation> ReservationFkFirstMember { get; set; }
-        public virtual ICollection<Reservation> ReservationFkSecondMember { get; set; }
+        public virtual ICollection<Reservation> ReservationFkSecondMember { get; set; } 
+        #endregion
     }
 }
--- HTK.Entities/Models/Ranking.cs
+++ HTK.Entities/Models/Ranking.cs
@@ -1,11 +1,89 @@
-﻿namespace HTK.Entities.Models
+﻿using HTK.Utilities;
+using System;
+
+namespace HTK.Entities.Models
 {
     public partial class Ranking
     {
-        public int PkRankId { get; set; }
-        public int FkMemberId { get; set; }
-        public int Points { get; set; }
+        #region Fields
+        protected int pkRankId;
+        protected int fkMemberId;
+        protected int points; 
+        #endregion
 
-        public virtual Member FkMember { get; set; }
+        #region Properties
+        /// <summary>
+        /// Id of the <see cref="Ranking"/>
+        /// </summary>
+        public virtual int PkRankId
+        {
+            get { return pkRankId; }
+            set
+            {
+                if(pkRankId != value)
+                {
+                    (bool isValid, string errorMessage) = Validations.ValidateIsIntNegative(value);
+                    if(isValid)
+                    {
+                        pkRankId = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(PkRankId));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Id of the <see cref="Ranking"/> <see cref="Member"/>
+        /// </summary>
+        public virtual int FkMemberId
+        {
+            get { return fkMemberId; }
+            set
+            {
+                if(fkMemberId != value)
+                {
+                    (bool isValid, string errorMessage) = Validations.ValidateIsIntNegative(value);
+                    if(isValid)
+                    {
+                        fkMemberId = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(FkMemberId));
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Points of the <see cref="Ranking"/> <see cref="Member"/>
+        /// </summary>
+        public virtual int Points
+        {
+            get { return points; }
+            set
+            {
+                if(points != value)
+                {
+                    (bool isValid, string errorMessage) = Validations.ValidateIsIntNegative(value);
+                    if(isValid)
+                    {
+                        points = value;
+                    }
+                    else
+                    {
+                        throw new ArgumentException(errorMessage, nameof(Points));
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Navigation Properties
+        public virtual Member FkMember { get; set; } 
+        #endregion
     }
 }
