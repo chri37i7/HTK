a10ab258fd619a3a1b491321efe5a74c73ede215
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-09-24 11:44:06 +0200

    More reservation stuff

--- HTK.Desktop.Gui/ViewModels/ReservationViewModel.cs
+++ HTK.Desktop.Gui/ViewModels/ReservationViewModel.cs
@@ -1,15 +1,86 @@
 ﻿using HTK.DataAccess;
+using HTK.DataAccess.Base;
 using HTK.DataAccess.Factory;
 using HTK.Desktop.Gui.ViewModels.Base;
 using HTK.Entities.Models;
 using HTK.Utilities;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 
 namespace HTK.Desktop.Gui.ViewModels
 {
     public class ReservationViewModel : ViewModelBase<Reservation>
     {
+        #region Fields
+        protected ObservableCollection<Court> courts;
+        protected Court selectedCourt;
+        protected ObservableCollection<Member> members;
+        protected Member selectedMember;
+        #endregion
+
+        public ReservationViewModel()
+        {
+            courts = new ObservableCollection<Court>();
+            members = new ObservableCollection<Member>();
+        }
+
+        #region Properties
+
+        #region Courts
+        /// <summary>
+        /// The courts in the Courts ComboBox in the view
+        /// </summary>
+        public ObservableCollection<Court> Courts
+        {
+            get { return courts; }
+            set
+            {
+                SetProperty(ref courts, value);
+            }
+        }
+
+        /// <summary>
+        /// The selected Court of the Courts ComboBox in the view
+        /// </summary>
+        public virtual Court SelectedCourt
+        {
+            get { return selectedCourt; }
+            set
+            {
+                SetProperty(ref selectedCourt, value);
+            }
+        }
+        #endregion
+
+        #region Members
+        /// <summary>
+        /// The members in the view available for selection when making a reservation
+        /// </summary>
+        public ObservableCollection<Member> Members
+        {
+            get { return members; }
+            set
+            {
+                SetProperty(ref members, value);
+            }
+        }
+
+        /// <summary>
+        /// The selected Member in the view
+        /// </summary>
+        public virtual Member SelectedMember
+        {
+            get { return selectedMember; }
+            set
+            {
+                SetProperty(ref selectedMember, value);
+            }
+        } 
+        #endregion
+
+        #endregion
+
         #region Methods
         /// <summary>
         /// Override of <see cref="LoadAllAsync"/> to use <see cref="ReservationRepository"/> instead for including navigation properties
@@ -25,6 +96,16 @@ namespace HTK.Desktop.Gui.ViewModels
             IEnumerable<Reservation> reservations = await reservationRepository.GetAllAsync();
             // Replace collection
             Items.ReplaceWith(reservations);
+
+
+            // Create factory, and get the instance
+            RepositoryFactory<RepositoryBase<Court>, Court> courtFactory = RepositoryFactory<RepositoryBase<Court>, Court>.GetInstance();
+            // Create repository with the factory
+            RepositoryBase<Court> courtRepository = courtFactory.Create();
+            // Get all reservations
+            IEnumerable<Court> courts = await courtRepository.GetAllAsync();
+            // Replace collection
+            Courts.ReplaceWith(courts);
         }
         #endregion
     }
