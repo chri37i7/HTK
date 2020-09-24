7fa9e592a089fd1c5f18bc5d492aa623601dc4ba
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-09-24 14:51:26 +0200

    Last commit

--- HTK.WebApi/Startup.cs
+++ HTK.WebApi/Startup.cs
@@ -19,6 +19,8 @@ namespace HTK.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+    
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
