3f22e95bbe5282de285c879da1f39ee1301a1c91
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-09-24 14:18:17 +0200

    More tests

--- HTK.Tests/RepositoryTests.cs
+++ HTK.Tests/RepositoryTests.cs
@@ -52,5 +52,125 @@ namespace HTK.Tests
             Assert.IsTrue(members.Count() > 0);
         }
         #endregion
+
+        #region Court Tests
+        /// <summary>
+        /// Tests that a court returned by id is not null
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public virtual async Task GetCourtById()
+        {
+            // Arrange
+            RepositoryBase<Court> repository;
+            Court court;
+
+            // Act
+            repository = new RepositoryBase<Court>();
+            court = await repository.GetByIdAsync(1);
+
+            // Assert
+            Assert.IsNotNull(court);
+        }
+
+        /// <summary>
+        /// Tests that all courts are returned
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public virtual async Task GetAllCourts()
+        {
+            // Arrange
+            RepositoryBase<Court> repository;
+            IEnumerable<Court> courts;
+
+            // Act
+            repository = new RepositoryBase<Court>();
+            courts = await repository.GetAllAsync();
+
+            // Assert
+            Assert.IsTrue(courts.Count() > 0);
+        }
+        #endregion
+
+        #region Ranking Tests
+        /// <summary>
+        /// Tests that a rankings returned by id is not null
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public virtual async Task GetRankingById()
+        {
+            // Arrange
+            RepositoryBase<Ranking> repository;
+            Ranking rankings;
+
+            // Act
+            repository = new RepositoryBase<Ranking>();
+            rankings = await repository.GetByIdAsync(1);
+
+            // Assert
+            Assert.IsNotNull(rankings);
+        }
+
+        /// <summary>
+        /// Tests that all rankings are returned
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public virtual async Task GetAllRankings()
+        {
+            // Arrange
+            RepositoryBase<Ranking> repository;
+            IEnumerable<Ranking> rankings;
+
+            // Act
+            repository = new RepositoryBase<Ranking>();
+            rankings = await repository.GetAllAsync();
+
+            // Assert
+            Assert.IsTrue(rankings.Count() > 0);
+        }
+        #endregion
+
+        #region Ranking Tests
+        /// <summary>
+        /// Tests that a reservation returned by id is not null
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public virtual async Task GetReservationById()
+        {
+            // Arrange
+            RepositoryBase<Reservation> repository;
+            Reservation reservation;
+
+            // Act
+            repository = new RepositoryBase<Reservation>();
+            reservation = await repository.GetByIdAsync(1);
+
+            // Assert
+            Assert.IsNotNull(reservation);
+        }
+
+        /// <summary>
+        /// Tests that all reservations are returned
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public virtual async Task GetAllReservations()
+        {
+            // Arrange
+            RepositoryBase<Reservation> repository;
+            IEnumerable<Reservation> reservations;
+
+            // Act
+            repository = new RepositoryBase<Reservation>();
+            reservations = await repository.GetAllAsync();
+
+            // Assert
+            Assert.IsTrue(reservations.Count() > 0);
+        }
+        #endregion
     }
 }
